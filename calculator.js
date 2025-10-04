0652f0e48a6780ad64445ac32bff97a44b6c0ef0
Daniel Kofi Yamoah <dnnkofi@gmail.com>
2025-10-04 21:46:41 +0000

    added 4 arithmetic fucntions

--- calculator.js
+++ calculator.js
@@ -1,4 +1,22 @@
+function add( number1, number2) {
 
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    if (number2 === 0) {
+        return 'Division by zero error';
+    }
+    return number1 / number2;
+}
 
 
 document.getElementById('add').addEventListener('click', function() {
